ce9a86a2e9e2c5381f09588dd81007b267010c5a
GABRIEL RN <85408530+GaratujaBR@users.noreply.github.com>
2024-10-10 19:43:28 -0300

    Update algoritmos.py

--- algoritmos.py
+++ algoritmos.py
@@ -1,6 +1,8 @@
 import random
 import time
 
+random.seed(None)
+
 def gerar_dados_conteineres(num_conteineres):
     return [
         [random.randint(10, 50), random.randint(50, 200), random.randint(30, 100)]
